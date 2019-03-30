6f98df5293740bca1a1dc47b91b946690b1c93d5
Peter Zhigalov <peter.zhigalov@gmail.com>
2019-03-31 06:05:32 +0700

    Revert "macOS: Update all deprecated enums to their 10.12+ equivalents"
    This reverts commit 556beea4c373395e16aa00ae7e5e5db8e6f46468.
    
    Change-Id: Ib36efd9faae5c8b49115904f6b289fd673031843

--- src/plugins/platforms/cocoa/qnswindow.mm
+++ src/plugins/platforms/cocoa/qnswindow.mm
@@ -52,13 +52,13 @@ Q_LOGGING_CATEGORY(lcQpaEvents, "qt.qpa.events");
 static bool isMouseEvent(NSEvent *ev)
 {
     switch ([ev type]) {
-    case NSEventTypeLeftMouseDown:
-    case NSEventTypeLeftMouseUp:
-    case NSEventTypeRightMouseDown:
-    case NSEventTypeRightMouseUp:
-    case NSEventTypeMouseMoved:
-    case NSEventTypeLeftMouseDragged:
-    case NSEventTypeRightMouseDragged:
+    case NSLeftMouseDown:
+    case NSLeftMouseUp:
+    case NSRightMouseDown:
+    case NSRightMouseUp:
+    case NSMouseMoved:
+    case NSLeftMouseDragged:
+    case NSRightMouseDragged:
         return true;
     default:
         return false;
@@ -322,7 +322,7 @@ OSStatus CGSClearWindowTags(const CGSConnectionID, const CGSWindowID, int *, int
 
 - (NSColor *)backgroundColor
 {
-    return self.styleMask == NSWindowStyleMaskBorderless ?
+    return self.styleMask == NSBorderlessWindowMask ?
         [NSColor clearColor] : [super backgroundColor];
 }
 
