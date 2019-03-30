f7ae7a851ef711538d4f34c909232c9d58783591
Peter Zhigalov <peter.zhigalov@gmail.com>
2019-03-31 06:05:32 +0700

    Revert "macOS: Update all deprecated enums to their 10.12+ equivalents"
    This reverts commit 556beea4c373395e16aa00ae7e5e5db8e6f46468.
    
    Change-Id: I18cef4da1081ca98dcd70ba77bccce6370120ea6

--- src/plugins/platforms/cocoa/qnsview_mouse.mm
+++ src/plugins/platforms/cocoa/qnsview_mouse.mm
@@ -103,24 +103,25 @@
     if (!m_platformWindow)
         return;
 
-    switch (theEvent.type) {
-    case NSEventTypeLeftMouseDown:
+    NSEventType ty = [theEvent type];
+    switch (ty) {
+    case NSLeftMouseDown:
         m_frameStrutButtons |= Qt::LeftButton;
         break;
-    case NSEventTypeLeftMouseUp:
+    case NSLeftMouseUp:
          m_frameStrutButtons &= ~Qt::LeftButton;
          break;
-    case NSEventTypeRightMouseDown:
+    case NSRightMouseDown:
         m_frameStrutButtons |= Qt::RightButton;
         break;
-    case NSEventTypeRightMouseUp:
+    case NSRightMouseUp:
         m_frameStrutButtons &= ~Qt::RightButton;
         break;
-    case NSEventTypeOtherMouseDown:
-        m_frameStrutButtons |= cocoaButton2QtButton(theEvent.buttonNumber);
+    case NSOtherMouseDown:
+        m_frameStrutButtons |= cocoaButton2QtButton([theEvent buttonNumber]);
         break;
-    case NSEventTypeOtherMouseUp:
-        m_frameStrutButtons &= ~cocoaButton2QtButton(theEvent.buttonNumber);
+    case NSOtherMouseUp:
+        m_frameStrutButtons &= ~cocoaButton2QtButton([theEvent buttonNumber]);
     default:
         break;
     }
@@ -658,7 +659,7 @@
         // knowing whether or not a NSEventPhaseEnded will be followed by a momentum phase.
         // The best we can do is to look at the event queue and hope that the system has
         // had time to emit a momentum phase event.
-        if ([NSApp nextEventMatchingMask:NSEventMaskScrollWheel untilDate:[NSDate distantPast]
+        if ([NSApp nextEventMatchingMask:NSScrollWheelMask untilDate:[NSDate distantPast]
                 inMode:@"QtMomementumEventSearchMode" dequeue:NO].momentumPhase == NSEventPhaseBegan) {
             Q_ASSERT(pixelDelta.isNull() && angleDelta.isNull());
             return; // Ignore this event, as it has a delta of 0,0
